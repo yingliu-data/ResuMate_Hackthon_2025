352ddc52e81ed51647155b48ed270efce3c94f60
Ying Liu <sophia17_2@hotmail.com>
2025-02-22 22:31:53 +0000

    test

--- data_scraper.py
+++ data_scraper.py
@@ -1,6 +1,5 @@
 # from langchain_community.document_loaders import SeleniumURLLoader
-
-# from github import Github
+from github import Github
 
 # def linkedin_data(url):
 #     loader = SeleniumURLLoader(urls=[url])
@@ -21,39 +20,19 @@ def github_data(username):
             "language": repo.language,
             "stars": repo.stargazers_count,
             "forks": repo.forks_count,
-            "labels": repo.get_labels(),
-            "issues": repo.get_issues(state="all"),
-            "contents": repo.get_contents(""),
-
-        })
-
-    return repo_info
-
-if __name__ == "__main__":
-    # result = linkedin_data("https://www.linkedin.com/in/yingliu-data/")
-    # print(result)
-
-    from github import Github
-
-    client = Github()
-
-    user = client.get_user("sophia172")
-    repos = user.get_repos()
-
-    repo_info = []
-    for repo in repos:
-        repo_info.append({
-            "name": repo.name,
-            "description": repo.description,
-            "language": repo.language,
-            "stars": repo.stargazers_count,
             "forks": repo.forks_count,
             "labels": repo.get_labels(),
             "issues": repo.get_issues(state="all"),
             "contents": repo.get_contents(""),
 
         })
+    return repos
+
+if __name__ == "__main__":
+    # result = linkedin_data("https://www.linkedin.com/in/yingliu-data/")
+    # print(result)
     import pickle
+
+    repos = github_data("sophia172")
     with open("github_info.pkl", "wb") as file:
-        pickle.dump(repo_info, file)
-    print(repo_info)
+        pickle.dump(repos, file)
